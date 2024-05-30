3add5ecaa0c908f01e6d7a601e437d92ac4a6603
emerick-biron <emerick.biron@gmail.com>
2024-05-30 12:16:40 +0200

    fix datetime Table title

--- src/elegantmotd/motd.py
+++ src/elegantmotd/motd.py
@@ -75,8 +75,10 @@ def generate_table() -> Table:
         show_header=False,
         box=None,
         title_justify="left",
-        title=f" System information as of {local_time.strftime(
-            '%a. %d %B %Y %H:%M:%S')} UTC+{utc_offset}\n",
+        title=(
+            f" System information as of {local_time.strftime('%a. %d %B %Y %H:%M:%S')} "
+            f"UTC+{utc_offset}\n"
+        ),
         title_style=Style(color="white", italic=False, bold=True, ),
         expand=True,
         leading=1,
